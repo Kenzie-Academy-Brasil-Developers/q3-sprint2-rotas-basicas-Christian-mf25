b08b136fa3b5943b09d6ff0d22ba8914b2da03d3
Christian <christian.mf22@gmail.com>
2021-12-15 20:26:50 -0300

    Refatorando codigo

--- app.py
+++ app.py
@@ -13,23 +13,16 @@ def name():
 
 @app.route("/current_datetime", methods=["GET"])
 def date_time():
+	date = datetime.now().strftime("%d/%m/%Y %H:%M:%S %P").upper()
 	hour = datetime.now().strftime("%H")
-	minutes = datetime.now().strftime("%M")
-	seconds = datetime.now().strftime("%S")
-	period = datetime.now().strftime("%P").upper()
-	day = datetime.now().strftime("%d")
-	month = datetime.now().strftime("%m")
-	year = datetime.now().strftime("%Y")
-	moment = ""
-	date_time = f"{day}/{month}/{year} {hour}:{minutes}:{seconds} {period}"
 
 	if int(hour) < 12:
-		moment += "Bom dia!"
+		moment = "Bom dia!"
 	elif int(hour) < 18:
-		moment += "Boa tarde!"
+		moment = "Boa tarde!"
 	else:
-		moment += "Boa noite!"
+		moment = "Boa noite!"
     
-	result = {"current_datetime": date_time, "message": moment}
+	result = {"current_datetime": date, "message": moment}
 
 	return result
